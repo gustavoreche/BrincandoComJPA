7335af13a2842010f55ba76cb6ba36312a2b706e
Gustavo Reche <idd_greche@uolinc.com>
2021-08-18 12:14:03 -0300

    Cria primeiro modelo

---
+++ loja/src/main/java/br/com/jpa/loja/modelo/Produto.java
@@ -0,0 +1,43 @@
+package br.com.jpa.loja.modelo;
+
+import java.math.BigDecimal;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "produtos")
+public class Produto {
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	private String nome;
+	
+	@Column(name = "descricao_produto")
+	private String descricao;
+	
+	private BigDecimal preco;
+
+	public Long getId() {
+		return id;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public String getDescricao() {
+		return descricao;
+	}
+
+	public BigDecimal getPreco() {
+		return preco;
+	}
+
+}
